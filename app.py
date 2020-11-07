9ee7f23595a28b3513a7a30ff7adfc2ec9b55dbd
alinwers <48154310+alinwers@users.noreply.github.com>
2020-11-07 17:25:07 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import json, pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -12,7 +14,10 @@ def get_data():
 
 @app.route("/catalogue/")
 def catalogue():
-    return render_template("catalog.html")
+    db = pd.read_json("http://127.0.0.1:5000/api/data")
+    temp = db.to_dict('records')
+    columnNames = db.columns.values
+    return render_template("catalog.html", records=temp, colnames=columnNames)
 
 @app.route("/graph/")
 def graph():
