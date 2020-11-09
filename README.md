e70e9b3c8eabc691a38c1090229791cc12804724
PamoSapph <63760657+PamoSapph@users.noreply.github.com>
2020-11-09 15:27:48 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # projet_systeme
-1. Install a virtual environment on the folder ```python -m venv env``` (Windows) or ```python3 -m venv env``` (Linux/Mac).
+1. Install a virtual environment ```python -m venv env``` (Windows) ```python3 -m venv env``` (Linux/Mac).
 2. Install all the required packages by doing ```pip install -r requirements.txt```.
-3. Run the venv ```.env\scripts\activate``` (Windows) or ```source env/bin/activate``` (Linux/Mac).
-4. Run the app ```python -m flask run``` (Windows) or ```python3 -m flask run``` (Linux/Mac).
--- app.py
+++ app.py
@@ -1,8 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import json, pandas as pd
 import os
+from math import trunc
+
+def truncate(nb,digits):
+    powerOfTen=10**digits
+    return trunc(nb*powerOfTen)/powerOfTen
 
 app = Flask(__name__)
+
+jsonFile = open("static/database.json","r")
+db = pd.read_json(jsonFile)
+records = db.to_dict('records')
+colNames = db.columns.values
+#displayedColNames = ["Host Name","Planet Letter","Planet Name","Discovery Method","Controversial Flag","Number of Planets in System","Orbital Period [days]","Orbit Semi-Major Axis [au]","Eccentricity","Inclination [deg]","Planet Mass or M*sin(i) [Jupiter Mass]","Planet Mass or M*sin(i) Provenance","Planet Radius [Jupiter radii]","Planet Density [g/cm**3]","TTV Flag","Kepler Field Flag","K2 Mission Flag","Number of Notes","RA [sexagesimal]","Dec [sexagesimal]","Distance [pc]","Gaia Distance [pc]","Optical Magnitude [mag]","Optical Magnitude Band","G-band (Gaia) [mag]","Effective Temperature [K]","Stellar Mass [Solar mass]","Stellar Radius [Solar radii]","Date of Last Update","Discovery Facility"]
+
+colTypes=[]
+for col in colNames:
+    for row in records:
+        if row[col]!="nan":
+            colTypes.append(type(row[col]).__name__)
+            break;
+colTypes[31]="str" #to avoid NoneType
 
 @app.route("/")
 def home():
@@ -12,13 +31,49 @@ def home():
 def get_data():
     return app.send_static_file("database.json")
 
-@app.route("/catalogue/")
-def catalogue():
-    db = pd.read_json("http://127.0.0.1:5000/api/data")
-    temp = db.to_dict('records')
-    columnNames = db.columns.values
-    return render_template("catalog.html", records=temp, colnames=columnNames)
+@app.route("/catalog/")
+def catalog():
+    return render_template("catalog.html",records=records,colNames=colNames)
+
+@app.route("/catalog/search/",methods=["GET","POST"])
+def search():
+    formData = request.args
+    searchInput = formData["searchInput"]
+    colName = formData["colName"]
+    comparator = formData["comparator"]
+    searchedRecords = []
+    colType = colTypes[list(colNames).index(colName)]
+    
+    if colType=="int":
+        searchInput=int(searchInput)
+        for record in records:
+            observedValue=record[colName]
+            if str(observedValue)!="nan":
+                if searchInput==observedValue:
+                    searchedRecords.append(record)
+
+    elif colType=="float":
+        searchInput=float(searchInput)
+        for record in records:
+            observedValue=record[colName]
+            if str(observedValue)!="nan":
+                nbDecimales=str(searchInput)[::-1].find('.')
+                if searchInput==truncate(observedValue,nbDecimales):
+                    searchedRecords.append(record)
+                    
+    elif colType=="str":
+        for record in records:
+            if searchInput in record[colName]:
+                searchedRecords.append(record)
+                
+    return render_template("catalog.html",records=searchedRecords,colNames=colNames)
+
+@app.route("/record/")
+def record():
+    return "record"
 
 @app.route("/graph/")
 def graph():
     return render_template("graph.html")
+
+app.run()
